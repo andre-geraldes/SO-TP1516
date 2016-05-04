bb976ab460803795a97578c8ffa78c193ce1cb25
BBarbosa <brunobarbosa.caos@gmail.com>
2016-05-04 17:52:39 +0100

    added some stuff, not tested yet

--- client.c
+++ client.c
@@ -1,6 +1,38 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<fcntl.h>
 
+char *fifo = "/home/barbosa/.Backup/fifo";
+
+// client main
 int main(int argc, char const *argv[]) {
     /* code */
+    char b;
+    int fd;
+
+    // at least, it must have the executable and the operation
+    if(argc < 2) {
+      printf("Syntax Error: Try sobucli [ backup | restore ]\n");
+      exit(1);
+    }
+
+    // tries to open the pipe
+    if((fd = open(fifo,O_WRONLY)) == -1) {
+      printf("Error: Could not open the pipe!\n")
+      exit(1);
+    }
+
+    // checks operation
+    if(strcmp(argv[1],"backup") != 0 && strcmp(argv[1],"restore") != 0) {
+      printf("Syntax Error: Try sobucli [ backup | restore ]\n");
+      exit(1);
+    }
+
+    close(fd);
+
     return 0;
 }
--- server.c
+++ server.c
@@ -1,6 +1,33 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<fcntl.h>
+
+#define SIZE 512
+
+char *fifo = "/home/barbosa/.Backup/fifo";
 
 int main(int argc, char const *argv[]) {
-    /* code */
+    int read,fd;
+    char b[SIZE];
+
+    // creates named pipe
+    if(mkfifo(fifo,0666) == -1) {
+      printf("Error: Could not create the named pipe!\n");
+      exit(1);
+    };
+
+    // reads continualy from the named pipe
+    while(1) {
+      fd = open(fifo,O_RDONLY);
+      // ends with Ctrl + D
+      while((read = read(fd,&b,SIZE)) > 0) {
+        write(1,&b,read);
+      }
+      close(fd);
+    }
     return 0;
 }
