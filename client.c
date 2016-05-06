2e2d4bf6b3e086f74549e04f52cb8f9d137945bd
BBarbosa <brunobarbosa.caos@gmail.com>
2016-05-06 17:35:20 +0100

    signal not working

--- client.c
+++ client.c
@@ -9,12 +9,23 @@
 
 #define SIZE 512
 
+int result = -1; // variable that confirms the operation
+
+// handler function
+void hand(int s) {
+  if(s == SIGUSR1) {
+    result = 0; // successfully completed
+  } else if (s == SIGUSR2) {
+    result = 1; // something wen't wrong
+  }
+}
+
 // client main
 int main(int argc, char const *argv[]) {
     // set fifo location
     char *fifo = strcat(getenv("HOME"),"/.Backup/fifo");
-    char command[512];
-    int fd,i;
+    char command[SIZE];
+    int fd,i,l;
 
     // at least, it must have the executable and the operation
     if(argc < 2) {
@@ -28,25 +39,36 @@ int main(int argc, char const *argv[]) {
       exit(1);
     }
 
-    // checks operation
+    // checks if the operation is valid
     if(strcmp(argv[1],"backup") != 0 && strcmp(argv[1],"restore") != 0) {
       printf("Syntax Error: Try sobucli [ backup | restore ]\n");
       exit(1);
     }
 
     // concatenates argvs into one command
-    strcpy(command,argv[1]);
+    sprintf(command,"%d\t%s",getppid(),argv[1]);
     for(i=2;i<argc;i++) {
-      strcat(command," ");
-      strcat(command,argv[i]);
+      // verifies if the file exists
+      sprintf(command,"%s %s",command,argv[i]);
     }
-    command[strlen(command)] = '\n';
-    command[strlen(command)] = '\0';
+    l = sprintf(command,"%s\n",command);
 
     // send command
-    write(fd,command,strlen(command)-1);
+    write(fd,command,l);
 
     // wait for signal
+    signal(SIGUSR1,hand);
+    signal(SIGUSR2,hand);
+    pause();
+
+    // receives the signal
+    if(result == 0) { // successfully done
+      puts("Good");
+    } else if (result == 1) { // something wen't wrong
+      puts("Bad");
+    } else { // nothing happened
+      puts("Nothing");
+    }
 
     // close pipe descriptor
     close(fd);
